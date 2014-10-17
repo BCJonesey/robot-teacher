1f4546276a3fb939bf77760d7b3501d9ae93244f
Ben Jones <ben@cerberusentertainment.com>
2014-10-17 07:38:05 -0400

    first rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RobotTeacher::Application.config.secret_token = '1dee7b746610f3570e251c6b6b6dc418acc392e2bb1af0a47e800f4c5d6529719041a5f7e12a37d63ee3e931811c02050ab7bffcdc8d07689c86f479b0858272'
